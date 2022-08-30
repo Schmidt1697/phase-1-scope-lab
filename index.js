6360be0925f9a4c48e491ad093e11e89c98f76f5
Anna Schmidt <marshall1697@gmail.com>
2022-08-30 09:23:52 -0600

    commit tree fb380dc8aca4ecb0b42d43a7b4ea8ba352aeb339

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+   return
+};
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return
+};
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'irvin'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'john';
+}
